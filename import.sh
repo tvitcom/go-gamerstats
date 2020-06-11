0e07da0e07d03624883258682e33733da0df8007
Roman V <tvitcom@yandex.ru>
2020-06-11 05:07:00 +0300

    add import user table

--- import.sh
+++ import.sh
@@ -2,6 +2,6 @@
 ## run for impot into mongo db
 export $(grep -v '^#' .env | xargs -d '\n')
 #echo $db_name
-#mongoimport --host ${db_host} --port ${db_port} --db ${db_name} --collection users --drop --file ./users_go.json --batchSize 1
+mongoimport --host ${db_host} --port ${db_port} --db ${db_name} --collection users --drop --file ./users_go.json --batchSize 1
 mongoimport --host ${db_host} --port ${db_port} --db ${db_name} --collection user_games --drop --file ./data/games.json --batchSize 1
 
